d757edc4848500c633da4718698a1ce773f78be6
이상희 <tkdhi0107@gmail.com>
2025-06-02 16:03:59 +0900

    백엔드 추가

---
+++ backend/src/main/kotlin/com/example/app/backend/SampleController.kt
@@ -0,0 +1,13 @@
+package com.example.app.backend
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class SampleController {
+
+    @GetMapping("/hello")
+    fun sayHello(): String {
+        return "Hello, Sanghee6"
+    }
+}
---
+++ backend/src/test/kotlin/com/example/app/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.app.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
